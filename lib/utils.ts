720b9962a55c3119b09fb3da62ad4feea1e4debb
Sean <seanlengws@gmail.com>
2023-06-09 22:08:21 +0800

    fix: updated utils and added metadata to intro to llm

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,8 @@
+import fs from 'fs'
 import fn from 'funclib'
 import parser from 'dir-parser'
+
+export interface RootProjectMetadata {}
 
 /**
  * Generate metadata based on a given path
@@ -16,7 +19,30 @@ export const directoryParser = async (path: string) => {
     includes: [],
   })
 
-  console.log(fn.pretty(directoryParser.children))
+  return fn.pretty(directoryParser.children)
 }
 
-directoryParser('projects')
+/**
+ * Write data to a JSON file
+ * @param fileName - The name you want your file to be in.
+ * @param filePath - The path you want to save your file in
+ * @param data - The data you want to write into the file
+ */
+export const writeJson = (fileName: string, filePath: string, data: RootProjectMetadata) => {
+  fs.writeFile(`${filePath}/${fileName}.json`, JSON.stringify(data), (err) => {
+    if (err) {
+      console.error('Error writing file', err)
+    } else {
+      console.log('Successfully wrote file')
+    }
+  })
+}
+
+export const createMetaDataFile = async () => {
+  const PROJECTS_DIRECTORY_PATH = './projects'
+
+  const [directoryTree] = await directoryParser(PROJECTS_DIRECTORY_PATH)
+  console.log(directoryTree)
+}
+
+createMetaDataFile()
